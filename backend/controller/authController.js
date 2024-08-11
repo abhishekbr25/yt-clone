920ddc76ef6d3d1ef42dc4ce4928c1232bedff51
Abhishek Kumar <aadarshraj7094@gmail.com>
2024-08-11 22:29:50 +0530

    added login logic with bcrypt

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,5 +14,20 @@ export const signup = async (req, res, next) => {
   if (!user) res.json({ msg: "signup failed" });
   res.json({ msg: "signed in success" });
 
+  next();
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) res.json({ msg: "all feilds required" });
+
+  const user = await User.findOne({ email });
+  if (!user) return res.json({ msg: "invalid email" });
+
+  const auth = await bcrypt.compare(password, user.password);
+
+  if (!auth) return res.json({ msg: "incorrect password" });
+
+  return res.json({ msg: "user logged in successfully", success: true });
   next();
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -12,7 +13,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    
+
     password: {
       type: String,
       required: true,
@@ -27,5 +28,9 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User =  mongoose.model("User", userSchema);
-export default User
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
