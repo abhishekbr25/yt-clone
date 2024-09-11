0feb72f64db62ecd1c7025d1bb63bc5bf938d4e3
Abhishek Kumar <aadarshraj7094@gmail.com>
2024-09-11 23:42:06 +0530

    post page ready

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,14 +1,47 @@
-export function PostCard({src}) {
+import { Bookmark, Heart, MessageSquare } from "lucide-react";
+import { useCallback, useState } from "react";
+
+export function PostCard({ src }) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const toggleLike = useCallback(() => {
+    setIsLiked(!isLiked);
+  }, [isLiked]);
+
+  const toggleBookmarked = useCallback(() => {
+    setIsBookmarked(!isBookmarked);
+  }, [isBookmarked]);
+
   return (
     <div className="bg-slate-900 max-w-4xl mx-auto mb-4 py-4 rounded-xl">
       <div className="flex flex-col items-center gap-3 max-w-xl mx-auto">
         <div className="actualpost ">
-            {/* post form backend */}
-          <img src={src} />
+          {/* post form backend */}
+          <div className=" bg-black ">
+            <img src={src} className="w-[720px] h-[480px] object-contain" />
+          </div>
         </div>
         <div className="userInteractivityIcons flex justify-between w-full text-white px-2">
-          <div>like --- comment</div>
-          <div>Save</div>
+          <div className="text-red-900 flex w-1/5 justify-between">
+            <div>
+              <Heart
+                fill={`${isLiked ? "red" : "none"}`}
+                color={`${isLiked ? "red" : "grey"}`}
+                onClick={toggleLike}
+              />
+            </div>
+            <div>
+              <MessageSquare />
+            </div>
+          </div>
+          <div>
+            <Bookmark
+              fill={`${isBookmarked ? "grey" : "none"}`}
+              color="grey"
+              onClick={toggleBookmarked}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -16,3 +49,5 @@ export function PostCard({src}) {
 }
 
 // {src}
+// img fixed h,w object contain to set aspect ratio accordingly
+//              fill={`${isLiked ? "red" : "grey"}`}
--- frontend/src/components/PostGrid.jsx
+++ frontend/src/components/PostGrid.jsx
@@ -2,7 +2,7 @@ import { PostCard } from "./PostCard";
 
 export function PostGrid() {
   return (
-    <div className="post-container bg-slate-800 px-40">
+    <div className="post-container bg-slate-800 px-40 py-2">
       <PostCard src={'/p2.png'}/>
       <PostCard src={'/post1.png'}/>
       <PostCard src={'p2.png'}/>
